b626c114ae355aac1ff162a49acdd1cc31a0ff97
Rachel Kang <rachelkang@microsoft.com>
2022-09-12 10:44:29 -0400

    Add DevReach demo

---
+++ 2022-09-12 The Journey to Accessible Apps with .NET MAUI/DevReachDemo/DevReachDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DevReachDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 2022-09-12 The Journey to Accessible Apps with .NET MAUI/DevReachDemo/DevReachDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DevReachDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
